4b7bc8fa75ea00a2203756eaacf9d88c0edfc35b
Melissa Perez <melissaalexperez@gmail.com>
2023-08-30 21:30:04 -0400

    I'm so frustrated rn lol

---
+++ index.js
@@ -0,0 +1,43 @@
+function add() {
+  // we'll fill this in shortly
+}
+function subtract() {
+  // we'll fill this in shortly
+}
+function multiply() {
+  // we'll fill this in shortly
+}
+function divide() {
+  // we'll fill this in shortly
+}function add(a, b) {
+  return a + b;
+}
+function subtract(a, b) {
+  return a - b;
+}
+function multiply(a, b) {
+  return a * b;
+}
+function divide(a, b) {
+  return a / b;
+} 
+
+function increment (a){
+    return a + 1;
+}
+function decrement (a){
+    return a - 1;
+}
+let number = a;
+number++;
+
+function makeInt(a) {
+    return parseInt(a,10)
+}
+function preserveDecimal (string) {
+    return parseFloat(string)
+}
+// function isNaN (NaN) {
+//     console.log(parseInt(sldkjflksjf));
+//     return toEqual (true)    
+// }
